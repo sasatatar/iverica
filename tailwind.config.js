ce1afe46460212014d1aa3b49fd8185551985a7d
Sasha Tatar <sasa.tatar@gmail.com>
2022-04-30 21:29:51 +0200

    Fix tailwind plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,5 @@ module.exports = {
     extend: {},
   },
   variants: {},
-  plugins: [
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
-    require("@tailwindcss/aspect-ratio"),
-  ],
+  plugins: [require("@tailwindcss/forms")],
 };
